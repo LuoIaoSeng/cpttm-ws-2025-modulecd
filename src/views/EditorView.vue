6571803e309fcaf8e4fd917110bbd2152b95ad77
in381-11-2025-cm <in381-11-2025-cm@student1.lan>
2025-11-09 12:41:19 +0800

    feat: add tool bar and serval tool images

--- src/views/EditorView.vue
+++ src/views/EditorView.vue
@@ -1,9 +1,7 @@
 <script setup>
 import { NumX, NumY } from '@/assets/config';
 import MapEditor from '@/components/MapEditor.vue';
-import { ref } from 'vue';
-
-
+import { onMounted, ref } from 'vue';
 
 const maps = ref([
     {
@@ -21,12 +19,69 @@ maps.value.forEach((obj) => {
 
 const mapIndex = ref(0)
 
+const tool = ref(-1)
+
+onMounted(() => {
+    window.addEventListener('mousedown', handleMouseDown)
+    window.addEventListener('contextmenu', (e) => {
+        e.preventDefault()
+    })
+})
+
+function handleMouseDown(e) {
+    // console.log(e)
+    if(e.button === 2) {
+        tool.value = -1
+    }
+}
+
 </script>
 
 <template>
 
-    <MapEditor :map="maps[mapIndex].data"></MapEditor>
+    <div class="tool">
+        <button class="tool-button" @click="tool = 2">
+            <img src="/src/assets/Spawn.svg" alt="">
+        </button>
+        <button class="tool-button" @click="tool = 3">
+            <img src="/src/assets/Star.svg" alt="">
+        </button>
+        <button class="tool-button" @click="tool = 4">
+            <img src="/src/assets/Spring.svg" alt="">
+        </button>
+        <button class="tool-button" @click="tool = 0">
+            <img src="/src/assets/Erase.svg" alt="">
+        </button>
+    </div>
+
+    <MapEditor :map="maps[mapIndex].data" :tool="tool"></MapEditor>
+
+    {{ tool }}
 
 </template>
 
-<style scoped></style>
+<style scoped>
+.tool {
+    display: flex;
+}
+
+.tool-button {
+    width: 50px;
+    height: 50px;
+    outline: none;
+    border: none;
+}
+
+.tool-button:hover {
+    background: #ccc;
+}
+
+.tool-button:active {
+    background: #ddd;
+}
+
+.tool-button > img {
+    width: 100%;
+    height: 100%;
+}
+</style>
