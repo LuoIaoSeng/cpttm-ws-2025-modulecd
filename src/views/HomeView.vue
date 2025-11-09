e0e60bf5730954e31cee1ad9ce0e52ad90accd56
in381-11-2025-cm <in381-11-2025-cm@student1.lan>
2025-11-09 11:08:42 +0800

    chore: init and create home page

---
+++ src/views/HomeView.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="app">
+        <div class="container">
+            <h1>Home Page</h1>
+            <RouterLink class="item">Map Editor</RouterLink>
+            <RouterLink class="item">Instructions</RouterLink>
+            <RouterLink class="item">Load Map File</RouterLink>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.app {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.container {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+}
+
+.item {
+    border: 2px solid #333;
+    padding: 1rem 4rem 1rem 2rem;
+    box-sizing: border-box;
+}
+</style>
